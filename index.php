38726a164bb6cad8ea6319fbfb1fd54858381d7e
HudalovOleg1988 <55542403+HudalovOleg1988@users.noreply.github.com>
2022-09-21 19:35:07 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+  include $_SERVER['DOCUMENT_ROOT'] . "/core/pdo.php";
+  include $_SERVER['DOCUMENT_ROOT'] . "/core/controller.php";
+  include $_SERVER['DOCUMENT_ROOT'] . "/core/route.php";
+  Route::start();
+?>
