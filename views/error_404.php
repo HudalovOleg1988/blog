1aa4419098e857327b6fd5b6ca0ec374e1d07756
HudalovOleg1988 <55542403+HudalovOleg1988@users.noreply.github.com>
2022-09-22 13:26:34 +0300

    Add files via upload

---
+++ views/error_404.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet/less" type="text/css" href="/less/reset.less">
+    <link rel="stylesheet/less" type="text/css" href="/less/header.less">
+    <link rel="stylesheet/less" type="text/css" href="/less/error_404.less">
+    <script src="https://cdn.jsdelivr.net/npm/less" ></script>
+    <title>Main</title>
+  </head>
+  <body>
+    <?php include $_SERVER['DOCUMENT_ROOT'] . "/views/html_include/header.php"; ?>
+    <div class="error_404"></div>
+    <p>Page not found!</p>
+  </body>
+</html>
---
+++ views/main.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet/less" type="text/css" href="/less/reset.less">
+    <link rel="stylesheet/less" type="text/css" href="/less/header.less">
+    <link rel="stylesheet/less" type="text/css" href="/less/text.less">
+    <script src="https://cdn.jsdelivr.net/npm/less" ></script>
+    <script src="/js/jquery.js" ></script>
+    <title>Main</title>
+  </head>
+  <body id="body">
+    <?php include $_SERVER['DOCUMENT_ROOT'] . "/views/html_include/header.php"; ?>
+    <?php include $_SERVER['DOCUMENT_ROOT'] . "/views/html_include/$html.php"; ?>
+    <script src="/js/open_menu.js" ></script>
+  </body>
+</html>
